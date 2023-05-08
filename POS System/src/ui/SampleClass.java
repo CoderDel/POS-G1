337fe41fd5c0c0804d0904ca1082f2ac3ab21be5
CodeSeemsFishy <88148858+CukooChick@users.noreply.github.com>
2023-05-08 20:48:13 +0800

    Add files via upload

---
+++ POS System/src/ui/SampleClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author User
+ */
+public class SampleClass {
+    private Color color;
+    
+    public SampleClass(Color color) {
+        this.color = color;
+    }
+}
